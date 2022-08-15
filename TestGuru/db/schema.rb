9d3ef87ea70a77f28b42c9e4451c5faaeff24297
kurchinm <kurchin.m@yandex.ru>
2022-08-15 10:14:33 +0300

    correct migrations

--- TestGuru/db/schema.rb
+++ TestGuru/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_13_151830) do
+ActiveRecord::Schema.define(version: 2022_08_15_070929) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
+    t.boolean "correct", default: true, null: false
+    t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "correct", default: true
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -27,15 +29,21 @@ ActiveRecord::Schema.define(version: 2022_08_13_151830) do
 
   create_table "questions", force: :cascade do |t|
     t.string "body", null: false
+    t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", default: 0
+    t.integer "level", default: 0, null: false
+    t.integer "category_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +52,8 @@ ActiveRecord::Schema.define(version: 2022_08_13_151830) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users"
 end
