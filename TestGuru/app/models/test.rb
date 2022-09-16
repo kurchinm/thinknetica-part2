8294d2e7489499cf07af5197edfaa9f788e02ca8
kurchinm <kurchin.m@yandex.ru>
2022-09-16 20:54:04 +0300

    error correction

--- TestGuru/app/models/test.rb
+++ TestGuru/app/models/test.rb
@@ -1,7 +1,10 @@
 class Test < ApplicationRecord
   class << self
     def test_by_category(category_name)
-      Test.joins('JOIN categories ON tests.category_id = categories.id').where(categories: {name: category_name}).order(name: :desc).pluck(:title)
+      joins('JOIN categories ON tests.category_id = categories.id')
+        .where(categories: {name: category_name})
+        .order(name: :desc)
+        .pluck(:title)
     end
   end
 end
--- TestGuru/app/models/user.rb
+++ TestGuru/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    TestResult.find_by(level: level, user_id: self.id)
+    Tests.joins('JOIN test_result ON test_result.test_id = tests.id')
+          .where(level: level, test_results: {user_id: self.id})
   end
 end
